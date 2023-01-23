52e94863a767c4351b18767edcbc8c8930dfa5d6
Leo5661 <singhra66@gmail.com>
2023-01-23 10:34:56 +0530

    added login api route

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,10 +3,10 @@ import bcrypt from "bcrypt";
 
 export const register = async (req, res, next) => {
     try{
-        console.log(req.body);
         const {username , email, password } = req.body;
         const usernameCheck = await userModel.findOne({ username });
         const emailCheck = await userModel.findOne({ email });
+
         if(usernameCheck){
             return res.json({ message: "User already present", status: false });
         }
@@ -19,6 +19,33 @@ export const register = async (req, res, next) => {
         const user = await userModel.create({
             username, email, password:hashPassword
         });
+
+        const userCopy = user.toObject();
+        delete userCopy['password'];
+        return res.json({ status: true, userCopy });
+    } catch (error) {
+        next(error);
+    }
+
+};
+
+export const login = async (req, res, next) => {
+    try{
+        const { username, password } = req.body;
+        const user = await userModel.findOne({ username });
+
+        console.log(user);
+        
+        if(!user){
+            return res.json({ message: "Username or password incorrect", status: false });
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.json({ message: "Username or password incorrect", status: false });
+        }
+
         const userCopy = user.toObject();
         delete userCopy['password'];
         return res.json({ status: true, userCopy });
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        select: false,
         min: 8,
     },
     isAvatarSet: {
